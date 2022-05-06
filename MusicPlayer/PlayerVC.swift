712ec6ab029d751a50e7721fd67b3065ad6742b1
shivaditya.thakur <shivaditya.thakur@bigohtech.com>
2022-05-06 10:49:21 +0530

    first commit

---
+++ MusicPlayer/PlayerVC.swift
@@ -0,0 +1,191 @@
+//
+//  PlayerVC.swift
+//  MusicPlayer
+//
+//  Created by Shivaditya Kumar on 15/03/22.
+//
+
+import UIKit
+import AVFoundation
+
+class PlayerVC: UIViewController {
+    public var position: Int = 0
+    public var songs: [Song] = []
+    @IBOutlet var holder: UIView!
+    var player:AVPlayer?
+    var playerItem:AVPlayerItem?
+    @IBOutlet var albumImageView : UIImageView!
+    @IBOutlet var songNameLabel : UILabel!
+    @IBOutlet var artistNameLabel : UILabel!
+    @IBOutlet var albumNameLabel : UILabel!
+    @IBOutlet var backButton : UIButton!
+    @IBOutlet var nextButton : UIButton!
+    @IBOutlet var playPauseButton : UIButton!
+    @IBOutlet var seekBack : UIButton!
+    @IBOutlet var seekForward : UIButton!
+    @IBOutlet var playbackSlider : UISlider!
+    @IBOutlet var startTimeLabel : UILabel!
+    @IBOutlet var endTimeLabel : UILabel!
+    @IBOutlet var stackView : UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        loadUI()
+        inActiveView()
+        DispatchQueue.main.asyncAfter(deadline: .now()) {
+            self.configure()
+        }
+    }
+    func activeView() {
+        stackView.alpha = 1
+    }
+    func inActiveView() {
+        stackView.alpha = 0.3
+    }
+    override func viewDidLayoutSubviews() {
+        
+    }
+    func loadUI(){
+        let song = songs[position]
+        albumImageView.image = UIImage(named: song.imageName)
+        //labels
+        songNameLabel.text = song.name
+        self.startTimeLabel.text = "--:--"
+        self.endTimeLabel.text = "--:--"
+        artistNameLabel.text = song.artistName
+        playbackSlider!.maximumValue = 0
+        playPauseButton.setTitle("", for: .normal)
+        backButton.setTitle("", for: .normal)
+        nextButton.setTitle("", for: .normal)
+        seekBack.setTitle("", for: .normal)
+        seekForward.setTitle("", for: .normal)
+        playPauseButton.layer.cornerRadius = 25
+        playPauseButton.backgroundColor = .black
+        albumImageView.layer.cornerRadius = 25
+        playPauseButton.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+        albumImageView.layer.shadowColor = UIColor.black.cgColor
+        albumImageView.layer.shadowOpacity = 1
+        albumImageView.layer.shadowOffset = CGSize.zero
+        albumImageView.layer.shadowRadius = 10
+    }
+    func configure(){
+        let song = songs[position]
+        let url = URL(string: song.trackName )
+        let playerItem:AVPlayerItem = AVPlayerItem(url: url!)
+        player = AVPlayer(playerItem: playerItem)
+        player?.play()
+        
+        // buttons
+        playPauseButton.addTarget(self, action: #selector(didPlayPauseButtonTapp), for: .touchUpInside)
+        backButton.addTarget(self, action: #selector(didBackButtonTapp), for: .touchUpInside)
+        nextButton.addTarget(self, action: #selector(didNextButtonTapp), for: .touchUpInside)
+        seekBack.addTarget(self, action: #selector(didSeekBackTap), for: .touchUpInside)
+        seekForward.addTarget(self, action: #selector(didSeekForwardTap), for: .touchUpInside)
+        // playback slider
+        playbackSlider!.minimumValue = 0
+        let duration : CMTime = playerItem.asset.duration
+        let seconds : Float64 = CMTimeGetSeconds(duration)
+        playbackSlider!.maximumValue = Float(seconds)
+        playbackSlider!.isContinuous = false
+        playbackSlider!.tintColor = .black
+        playbackSlider?.addTarget(self, action: #selector(self.playbackSliderValueChanged(_:)), for: .valueChanged)
+        player!.addPeriodicTimeObserver(forInterval: CMTimeMakeWithSeconds(1, preferredTimescale: 1), queue: DispatchQueue.main) { (CMTime) -> Void in
+            if self.player!.currentItem?.status == .readyToPlay {
+                let time : Float64 = CMTimeGetSeconds(self.player!.currentTime());
+                let endTime : Float64 = CMTimeGetSeconds(self.player!.currentItem!.duration);
+                self.playbackSlider!.value = Float ( time )
+                let secs = Int(time)
+                let endSecs = Int(endTime)
+                self.startTimeLabel.text = NSString(format: "%02d:%02d", secs/60, secs%60) as String
+                self.endTimeLabel.text = NSString(format: "%02d:%02d", endSecs/60, endSecs%60) as String
+                self.activeView()
+            }
+        }
+        
+    }
+   
+    @objc func playbackSliderValueChanged(_ playbackSlider:UISlider)
+    {
+        let seconds : Int64 = Int64(playbackSlider.value)
+        let targetTime:CMTime = CMTimeMake(value: seconds, timescale: 1)
+        player!.seek(to: targetTime)
+        if player!.rate == 0
+        {
+            player?.play()
+            playPauseButton.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+        } else {
+            playPauseButton.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+        }
+    }
+    private func changeValueForLabel(){
+//        startTimeLabel.text = String(round(self.player?.currentTime ?? 0.0))
+    }
+    @objc func didSeekBackTap(){
+        let time : Float64 = CMTimeGetSeconds(self.player!.currentTime())
+        let seconds : Int64 = Int64(round(time))
+        let targetTime:CMTime = CMTimeMake(value: seconds - 10, timescale: 1)
+        player!.seek(to: targetTime)
+        if player!.rate == 0
+        {
+            player?.play()
+            playPauseButton.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+        } else {
+            playPauseButton.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+        }
+    }
+    @objc func didSeekForwardTap(){
+        let time : Float64 = CMTimeGetSeconds(self.player!.currentTime())
+        let seconds : Int64 = Int64(round(time))
+        let targetTime:CMTime = CMTimeMake(value: seconds + 10, timescale: 1)
+        player!.seek(to: targetTime)
+        if player!.rate == 0
+        {
+            player?.play()
+            playPauseButton.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+        } else {
+            playPauseButton.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+        }
+    }
+    @objc func didNextButtonTapp(){
+        if position < (songs.count - 1) {
+            position = position + 1
+            player?.pause()
+            loadUI()
+            inActiveView()
+            DispatchQueue.main.asyncAfter(deadline: .now()) {
+                self.configure()
+            }
+        }
+    }
+    @objc func didBackButtonTapp(){
+        if position > 0 {
+            position = position - 1
+            player?.pause()
+            loadUI()
+            inActiveView()
+            DispatchQueue.main.asyncAfter(deadline: .now()) {
+                self.configure()
+            }
+        }
+    }
+    @objc func didPlayPauseButtonTapp(){
+        if player?.rate != 0
+        {
+            player!.pause()
+            playPauseButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        } else {
+            player!.play()
+            playPauseButton.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+        }
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if let player = player {
+            player.pause()
+            player.replaceCurrentItem(with: nil)
+            playPauseButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        }
+    }
+}
---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Shivaditya Kumar on 15/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView : UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        songs.append(Song(name: "Viva la da", albumName: "Album 3", artistName: "Cold Play", imageName: "2", trackName: "https://gmpdn.com/get/42937/aGgLXkqxdJtYqlO24GOEieX72E7kkDTgW1dNpMwgsmR/mp3"))
+        songs.append(Song(name: "Background music", albumName: "Album 1", artistName: "Rando", imageName: "1", trackName: "https://s3.amazonaws.com/kargopolov/kukushka.mp3"))
+        songs.append(Song(name: "Havana", albumName: "Album 2", artistName: "Camibla", imageName: "3", trackName: "https://gmpdn.com/get/23936/jWRVMbqY1nfNQLgx0RgKhgnx8jxaaoT4aq5xexXksk9/mp3"))
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    var songs = [Song]()
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songs.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for : indexPath)
+        let song = songs[indexPath.row]
+        cell.textLabel?.text = song.name
+        cell.detailTextLabel?.text = song.albumName
+        cell.accessoryType = .disclosureIndicator
+        cell.imageView?.contentMode = .scaleAspectFit
+        cell.imageView?.image = UIImage(named: song.imageName)
+        cell.textLabel?.font = UIFont( name: "Helvetica-Bold", size: 18)
+        cell.detailTextLabel?.font = UIFont( name: "Helvetica", size: 16)
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // present the player
+        let position = indexPath.row
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "player") as? PlayerVC else {
+            return
+        }
+        vc.songs = songs
+        vc.position = position
+        present(vc, animated: true)
+    }
+}
+struct Song {
+    let name : String
+    let albumName: String
+    let artistName: String
+    let imageName: String
+    let trackName: String
+}
